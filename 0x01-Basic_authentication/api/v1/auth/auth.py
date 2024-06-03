d9c40bcd2b199f6a0f36203edb9d70c73299c929
abdiwoli <abdiwolih@gmail.com>
2024-06-04 01:36:04 +0300

    task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,15 @@ class Auth:
     """ class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns Bool """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        if path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns str """

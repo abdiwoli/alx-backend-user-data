ce364a9c473af5e405963a1de1c76e6dee9e535b
abdiwoli <abdiwolih@gmail.com>
2024-06-12 17:30:00 +0300

    done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" db.py """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session

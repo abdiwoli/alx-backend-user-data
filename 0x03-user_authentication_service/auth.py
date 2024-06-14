587ffec9be37ebd8ccec1ef53dba1ab51ed52a43
abdiwoli <abdiwolih@gmail.com>
2024-06-15 00:03:50 +0300

    done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,10 +53,9 @@ class Auth:
         """ return session id """
         try:
             user = self._db.find_user_by(email=email)
-            if user:
-                session_id = _generate_uuid()
-                self._db.update_user(user.id, session_id=session_id)
-                return session_id
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except (ValueError, NoResultFound, InvalidRequestError):
             return None
 
